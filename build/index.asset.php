3e2e8b5b3c38e4b39ece331e6761adc0f32e35d7
Ronald Huereca <ronald@mediaron.com>
2024-04-09 05:45:53 -0500

    Changing category registration.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'b5ded88be68f47013485');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '97feab35bf36d9fe3aa4');
