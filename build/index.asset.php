4b4de1ed8775f71985fbc4819d549c3269fc5b81
Ronald Huereca <ronald@mediaron.com>
2024-05-22 15:50:48 -0500

    Updating icon.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '6f948d1ec5f1f8369ba5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '8acc3b91df9514a84ffc');
