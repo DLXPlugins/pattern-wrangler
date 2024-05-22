327b48c38ee59e3a567635557248b9a06692c2ee
Ronald Huereca <ronald@mediaron.com>
2024-05-22 15:39:06 -0500

    Updating icon in block.json.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '0af131fc34ef57820736');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '6f948d1ec5f1f8369ba5');
