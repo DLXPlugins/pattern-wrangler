dd1944ffcd005613e50416ae9cb315c6ae06c26b
Ronald Huereca <ronald@mediaron.com>
2025-11-29 21:27:29 -0600

    Removing tanstack router dependency.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => '0c6d5c9eb4341eba353b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-url'), 'version' => '134b5ca906a63eab1257');
--- src/js/react/views/patterns/index.js
+++ src/js/react/views/patterns/index.js
@@ -1,63 +1,12 @@
 import { render } from '@wordpress/element';
-import {
-	createHashHistory,
-	createRouter,
-	RouterProvider,
-	createRoute,
-	createRootRoute,
-} from '@tanstack/react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PatternsView from './components/PatternsView';
+import PatternsGrid from './components/PatternsGrid';
 import './styles/patterns-view.scss';
 
-// Create a hash history instance with the WordPress admin path
-const hashHistory = createHashHistory( {
-	initialEntries: [ '/' ],
-} );
-// Create the root route
-const rootRoute = createRootRoute( {
-	component: () => <PatternsView />,
-} );
-
-// Define your routes with different components or views
-const indexRoute = createRoute( {
-	getParentRoute: () => rootRoute,
-	path: '/',
-	component: PatternsView,
-} );
-
-// Local root.
-const pagedRoute = createRoute( {
-	getParentRoute: () => rootRoute,
-	path: '/paged',
-	component: PatternsView,
-} );
-
-const orderRoute = createRoute( {
-	getParentRoute: () => rootRoute,
-	path: '/order',
-	component: PatternsView,
-} );
-
-// Include all routes in the route tree
-const routeTree = rootRoute.addChildren( [ indexRoute, pagedRoute, orderRoute ] );
-
-// Create a router instance
-const router = createRouter( {
-	routeTree,
-	history: hashHistory,
-	context: {
-		history: hashHistory,
-	},
-} );
-
 const container = document.getElementById( 'dlx-pattern-wrangler-view' );
-const queryClient = new QueryClient();
+
 if ( container ) {
 	render(
-		<QueryClientProvider client={ queryClient }>
-			<RouterProvider router={ router } />
-		</QueryClientProvider>,
+		<PatternsGrid />,
 		container
 	);
 }
