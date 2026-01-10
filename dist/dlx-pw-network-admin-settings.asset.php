6638cac230526b98069e1c47e44b238d3b2c3c62
Ronald Huereca <ronald@mediaron.com>
2026-01-10 02:05:12 -0600

    Doing prod build.

--- dist/dlx-pw-network-admin-settings.asset.php
+++ dist/dlx-pw-network-admin-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-compose', 'wp-i18n', 'wp-keycodes', 'wp-url'), 'version' => 'c17184b5889627e00487');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-compose', 'wp-i18n', 'wp-keycodes', 'wp-url'), 'version' => '14610467a9de86d99cc7');
