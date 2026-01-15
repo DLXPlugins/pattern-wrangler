86d630bdf2dd9ab676453602bc650643d4a2b859
Ronald Huereca <ronald@mediaron.com>
2026-01-15 10:23:56 -0600

    Doing prod build.

--- dist/dlx-pw-admin.asset.php
+++ dist/dlx-pw-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => 'cf0cdb93705f714e5f91');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => '737ae91484e0a3a5129b');
