6adc95bc895615fc6363083979a2eabf1093b1fd
Ronald Huereca <ronald@mediaron.com>
2026-01-15 20:29:27 -0600

    Doing prod build.

--- build/dlx-pw-categories-view.asset.php
+++ build/dlx-pw-categories-view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => '31beeb53ad90ae5cb2d2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-url'), 'version' => 'd19e3ef53e6d96d81fbb');
