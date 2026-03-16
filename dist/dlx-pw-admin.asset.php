d75e529a7dd291182b462797bdff6eb15e3d4ab7
Ronald Huereca <ronald@mediaron.com>
2026-03-16 07:34:36 -0500

    Making it clear in the patterns screen what are links.

--- dist/dlx-pw-admin.asset.php
+++ dist/dlx-pw-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => '8d1ce550426fad6ced15');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-i18n'), 'version' => 'a1612f72c44820cbbd4a');
