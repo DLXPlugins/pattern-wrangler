76ec5db5b9d3d5e18feb0df9ee5aa120065cdbc1
Ronald Huereca <ronald@mediaron.com>
2026-04-15 00:41:34 -0500

    Adding more reliable back button to patterns screen.

--- src/js/blocks/plugins/pattern-preview.js
+++ src/js/blocks/plugins/pattern-preview.js
@@ -1,7 +1,10 @@
+import { useMemo, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { Button, Tooltip } from '@wordpress/components';
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginPostStatusInfo } from '@wordpress/editor';
+import { getQueryArg } from '@wordpress/url';
+import { subscribe } from '@wordpress/data';
 import './pattern-code.js';
 
 /**
@@ -53,3 +56,40 @@ const PatternPreviewButton = () => {
 registerPlugin( 'dlx-pattern-wrangler-preview-button', {
 	render: PatternPreviewButton,
 } );
+
+// This takes in a redirect_to query arg and stores it in localStorage. It then subscribes to the block editor data, and when the back button is finally rendered, it will redirect to the stored redirect_to URL.
+const RedirectToPatternsPage = () => {
+	const redirectTo = useMemo( () => getQueryArg( window.location.href, 'redirect_to' ), [] );
+	useEffect( () => {
+		if ( redirectTo ) {
+			localStorage.setItem( 'dlx-pw-redirect-to', redirectTo );
+		}
+	}, [ redirectTo ] );
+	return null;
+};
+
+registerPlugin( 'dlx-pattern-wrangler-redirect-to-patterns-page', {
+	render: RedirectToPatternsPage,
+} );
+
+/**
+ * Subscribe to data.
+ *
+ * @return {Function} The subscription function.
+ */
+const subscribeToData = () => {
+	const unsubscribe = subscribe( () => {
+		const backButton = document.querySelector( '.edit-post-fullscreen-mode-close' );
+		if ( backButton ) {
+			const redirecTo = localStorage.getItem( 'dlx-pw-redirect-to' );
+			if ( redirecTo ) {
+				backButton.href = decodeURIComponent( localStorage.getItem( 'dlx-pw-redirect-to' ) );
+				localStorage.removeItem( 'dlx-pw-redirect-to' );
+			}
+			unsubscribe();
+		}
+	} );
+	return unsubscribe;
+};
+
+subscribeToData();
