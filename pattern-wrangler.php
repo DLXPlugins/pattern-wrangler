42d410c8288e60d379dccc0f3bf0bf9cedbf7f43
Ronald Huereca <ronald@mediaron.com>
2026-05-28 22:04:42 -0500

    Fixing version.

--- pattern-wrangler.php
+++ pattern-wrangler.php
@@ -25,7 +25,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once __DIR__ . '/functions.php';
 
 define( 'DLXPW_PATTERN_WRANGLER_VERSION', '2.5.1' );
-define( 'DLXPW_PATTERN_WRANGLER_VERSION', '2.5.0' );
 define( 'DLXPW_PATTERN_WRANGLER_CACHE_VERSION', '0.0.4' );
 define( 'DLXPW_PATTERN_WRANGLER_FILE', __FILE__ );
 
